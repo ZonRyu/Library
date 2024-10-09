024fe6f6f95be8fa803d2bb31d09fde01070b2fc
ZonRyu <julyonchang@gmail.com>
2024-10-09 22:48:05 +0700

    Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { FaSearch } from "react-icons/fa"
+
+const Navbar = () => {
+  return (
+    <nav className="py-8 shadow-md">
+        <div className="container mx-auto flex justify-around items-center h-11">
+            <div className="text-xl font-bold mx-6">
+                <h1 className="">Library</h1>
+            </div>
+            <div className="relative flex-1 h-full mr-6">
+                <input type="text" className="px-5 border border-black rounded-3xl w-full h-full" placeholder="Search for book" />
+                <FaSearch className="absolute top-1/2 -translate-y-1/2 right-3" />
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
